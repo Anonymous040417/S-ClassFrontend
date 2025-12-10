df1d3f8e16d112a8337a54b1432e6c0c22f710fc
Anonynous040417 <karanjamkinyanjui0417@gmail.com>
2025-12-11 00:40:35 +0300

    yeepy

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'  
-// import { PersistGate } from 'redux-persist'  
+import { PersistGate } from 'redux-persist/es/integration/react'
 import { store, persistor } from './store/store'  
 import App from './App.tsx'
 import './index.css'
@@ -9,9 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}> 
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
